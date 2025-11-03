7aec4662097ca3075f0f929335c2760c4ff5910e
dengruihan <551310554@qq.com>
2025-11-03 12:32:51 +0800

    0.1

--- ajax/app.py
+++ ajax/app.py
@@ -7,7 +7,8 @@ students = {
     'S1': {'name': '张三', 'age': 18, 'grade': '高三', 'class': '1班'},
     'S2': {'name': '李四', 'age': 17, 'grade': '高二', 'class': '2班'},
     'S3': {'name': '王五', 'age': 18, 'grade': '高三', 'class': '3班'},
-    'S4': {'name': '赵六', 'age': 17, 'grade': '高二', 'class': '4班'}
+    'S4': {'name': '赵六', 'age': 17, 'grade': '高二', 'class': '4班'},
+    'S5': {'name': '赵七', 'age': 17, 'grade': '高二', 'class': '4班'}
 }
 
 @app.route('/')
